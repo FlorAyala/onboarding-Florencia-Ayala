8c7e58ef7cf68a22781b80d51ccec90aa81404f2
Florencia Ayala <flor.ayala30@gmail.com>
2024-03-19 16:33:29 -0300

    first commit

---
+++ introJS/main.js
@@ -0,0 +1,111 @@
+// console.log("Hola")
+//Difficulty Level: Low
+//1.
+const myName = "Florencia"
+console.log(myName)
+
+//2.
+const myLastName = "Ayala"
+console.log(myLastName)
+
+//3.
+let myAge = 28
+console.log(myAge)
+
+//4.
+let myPet = "Leon"
+console.log(myPet)
+
+//5.
+let petAge = 14
+console.log(petAge)
+
+//6.
+let fullName = ("My name is " + myName + " and my Last name is " + myLastName)
+console.log(fullName)
+
+//7.
+let presentationText = ("My name is " + myName + " and my Last name is " + myLastName + ". I also have a dog named " + myPet + " who is " + petAge + " yeards old")
+console.log(presentationText)
+
+
+//Difficulty Level: Medium
+//1.
+let sumAge = (myAge + petAge)
+console.log(sumAge)
+
+let subtractAges = (myAge - petAge)
+console.log(subtractAges)
+
+let productAges = (myAge * petAge)
+console.log(productAges)
+
+let divisionAges = (myAge / petAge)
+console.log(divisionAges)
+
+//2.
+let student = {
+    name: "Florencia",
+    lastName: "Ayala",
+    age: 28,
+    city: "Tandil",
+    contry: "Argentina"
+}
+console.table(student)
+
+
+//3.
+let pet = {
+    name: "Leon",
+    age: 14,
+    color: "brow",
+    size: "medium",
+    brothers: 1
+}
+console.table(pet)
+
+
+//4.
+let fruits = ["banana", "pera", "melon", "sandia", "uva"]
+console.log(fruits)
+console.log(fruits[0])
+console.log(fruits[1])
+console.log(fruits[2])
+console.log(fruits[3])
+console.log(fruits[4])
+
+
+//Difficulty Level: High
+
+//1.
+// let age = prompt("Enter your age:")
+// console.log(age)
+
+// let iamAdult = (18 < age)
+// console.log(iamAdult + " I am an adult " + age)
+
+//2.
+let numbers = [25, 13, 67, 85, 46]
+console.log(numbers)
+console.log(numbers[0])
+console.log(numbers[1])
+console.log(numbers[2])
+console.log(numbers[3])
+console.log(numbers[4])
+
+//3.
+let family = ["mamá", "papá", "hermana mayor", "hermana menor", "mascotas"]
+console.log(family)
+console.log(family[0])
+console.log(family[1])
+console.log(family[2])
+console.log(family[3])
+console.log(family[4])
+
+//4.
+
+
+
+
+
+
