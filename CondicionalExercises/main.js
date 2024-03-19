b6a298e139289d93edc2335615112d4eef440587
Florencia Ayala <flor.ayala30@gmail.com>
2024-03-19 20:24:33 -0300

    se empezo la carpeta de condiciones y se hizo hasta el ej 3 de los ej basico

---
+++ CondicionalExercises/main.js
@@ -0,0 +1,32 @@
+// console.log("hola")
+
+//Difficulty Level: Low
+//1.
+let num1 = prompt("ingrese el primero numero: ");
+let num2 = prompt("ingrese el segundo numero: ");
+
+// if (num1 >= num2){
+//     console.log("Number one is: " + num1)
+// }else{
+//     console.log("El segundo numero es: " + num2)
+// }
+
+
+//2.
+
+// if (num1 > num2) {
+//     console.log("Number one is: " + num1)
+// } else if (num1 < num2) {
+//     console.log("The second number is: " + num2)
+// } else {
+//     console.log("The numbers are equal")
+// }
+
+//3.
+if(num2 == num1){
+    console.log("The numbers are equal")
+}else{
+    console.log("The numbers are different")
+}
+
+
